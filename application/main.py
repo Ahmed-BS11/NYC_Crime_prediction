4394bfa61cca07a2be3fe9a73b45960e178fad3f
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-12-27 16:36:11 +0100

    main modif

--- application/main.py
+++ application/main.py
@@ -99,6 +99,20 @@ if input_method == "Text Input":
             folium.Marker(location=coordinates, popup=destination).add_to(base_map)
             # Display the map
             folium_static(base_map)
+            _, col, _ = st.columns(3)
+            with col:
+                predict = st.button("Predict", key="predict")
+            if predict:
+                if lat=='' or long == '' or precinct==None :
+                    st.error("Please make sure that you selected a location on the map")    
+                    if st.button("Okay"):
+                        pass
+                else:
+                    X = backend.create_df(date, hour, lat, long, place, age, race, gender, precinct, borough)
+                    pred, crimes = backend.predict(X)
+                    st.markdown(f"You are likely to be a victim of: **{pred}**")
+                    st.markdown(f"#### Some of the crimes types are the following: ")
+                    st.markdown(crimes)
         else:
             st.error("Unable to retrieve coordinates for the given destination.")
         
@@ -119,22 +133,17 @@ elif input_method == "Map Click":
     print(precinct,borough)
     st.success(f"Coordinates are {lat}: {long}")
     st.success(f'precinct = {precinct},borough ={borough} ')
-
-    
-
-
-
-_, col, _ = st.columns(3)
-with col:
-    predict = st.button("Predict", key="predict")
-if predict:
-    if lat=='' or long == '' or precinct==None :
-        st.error("Please make sure that you selected a location on the map")    
-        if st.button("Okay"):
-            pass
-    else:
-        X = backend.create_df(date, hour, lat, long, place, age, race, gender, precinct, borough)
-        pred, crimes = backend.predict(X)
-        st.markdown(f"You are likely to be a victim of: **{pred}**")
-        st.markdown(f"#### Some of the crimes types are the following: ")
-        st.markdown(crimes)
+    _, col, _ = st.columns(3)
+    with col:
+        predict = st.button("Predict", key="predict")
+    if predict:
+        if lat=='' or long == '' or precinct==None :
+            st.error("Please make sure that you selected a location on the map")    
+            if st.button("Okay"):
+                pass
+        else:
+            X = backend.create_df(date, hour, lat, long, place, age, race, gender, precinct, borough)
+            pred, crimes = backend.predict(X)
+            st.markdown(f"You are likely to be a victim of: **{pred}**")
+            st.markdown(f"#### Some of the crimes types are the following: ")
+            st.markdown(crimes)
