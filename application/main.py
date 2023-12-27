4e98880b5432e6505d630e5003a050926d9c357f
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-12-27 16:58:58 +0100

    main modif

--- application/main.py
+++ application/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import folium
-from streamlit_folium import st_folium,folium_static
+from streamlit_folium import st_folium
 from geopy.geocoders import Nominatim
 from datetime import datetime
 import backend as backend
