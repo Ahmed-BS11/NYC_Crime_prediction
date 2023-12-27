b074209fbfe1cc48889cbd192c1b01fd7b4c7bcd
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-12-27 16:54:13 +0100

    main modif

--- application/main.py
+++ application/main.py
@@ -87,7 +87,7 @@ if input_method == "Text Input":
     
     if st.button("Get Coordinates"):
         coordinates = get_coordinates(destination)
-        
+        lat, long = coordinates
         if coordinates:
             lat, long = coordinates
             precinct, borough = get_precinct_and_borough(lat, long)
@@ -103,27 +103,28 @@ if input_method == "Text Input":
             # Display the map
             map_container = st.empty()
             map_container.folium_static(base_map)
-            
-            _, col, _ = st.columns(3)
-            
-            with col:
-                predict = st.button("Predict", key="predict")
-                
-            if predict:
-                if lat == '' or long == '' or precinct is None:
-                    st.error("Please make sure that you selected a location on the map")
-                    
-                    if st.button("Okay"):
-                        pass
-                else:
-                    # Modify this part according to your data input
-                    X = backend.create_df(date, hour, lat, long, place, age, race, gender, precinct, borough)
-                    pred, crimes = backend.predict(X)
-                    st.markdown(f"You are likely to be a victim of: **{pred}**")
-                    st.markdown(f"#### Some of the crimes types are the following: ")
-                    st.markdown(crimes)
         else:
             st.error("Unable to retrieve coordinates for the given destination.")
+            
+        _, col, _ = st.columns(3)
+        
+        with col:
+            predict = st.button("Predict", key="predict")
+            
+        if predict:
+            if lat == '' or long == '' or precinct is None:
+                st.error("Please make sure that you selected a location on the map")
+                
+                if st.button("Okay"):
+                    pass
+            else:
+                # Modify this part according to your data input
+                X = backend.create_df(date, hour, lat, long, place, age, race, gender, precinct, borough)
+                pred, crimes = backend.predict(X)
+                st.markdown(f"You are likely to be a victim of: **{pred}**")
+                st.markdown(f"#### Some of the crimes types are the following: ")
+                st.markdown(crimes)
+        
 
 
 elif input_method == "Map Click":
